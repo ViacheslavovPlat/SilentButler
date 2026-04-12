227e8bd9f845f4862bba9c1cb4b5c707810978e1
Platon <viacheslavovplaton@.gmail.com>
2026-04-12 21:32:02 +0200

    added Auth dtos

---
+++ src/main/java/com/silentbutler/dto/RegisterRequest.java
@@ -0,0 +1,22 @@
+package com.silentbutler.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+    @NotBlank(message = "Username must bot be blank")
+    private String username;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password must not be blank")
+    private String passwrod;
+}
